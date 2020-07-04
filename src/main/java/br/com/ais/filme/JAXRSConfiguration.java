94fedad7ab672e4ba0ae46c6cc3ac4f454fc1ecd
Matheus Henrique Braga Caldeira <matheus.caldeira@maisvida.med.br>
2020-07-03 21:22:22 -0300

    projeto adicionado

---
+++ src/main/java/br/com/ais/filme/JAXRSConfiguration.java
@@ -0,0 +1,9 @@
+package br.com.ais.filme;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath(value = "api")
+public class JAXRSConfiguration extends Application {
+
+}
