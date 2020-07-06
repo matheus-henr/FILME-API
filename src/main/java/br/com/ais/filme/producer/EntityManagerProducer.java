07db21a879e3a2868469b8fb51f2da722d429d68
Matheus Henrique Braga Caldeira <matheus.caldeira@maisvida.med.br>
2020-07-06 15:39:15 -0300

    entity manager

--- src/main/java/br/com/ais/filme/producer/EntityManagerProducer.java
+++ src/main/java/br/com/ais/filme/producer/EntityManagerProducer.java
@@ -6,37 +6,25 @@ import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Disposes;
 import javax.enterprise.inject.Produces;
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
 import javax.persistence.PersistenceContext;
 
 @ApplicationScoped
-public class EntityManagerProducer  implements Serializable{
+public class EntityManagerProducer implements Serializable {
 
-	
 	private static final long serialVersionUID = 1L;
-	
-	@PersistenceContext
+
+	@PersistenceContext()
 	private EntityManager em;
 
-	 @Produces
-	 public EntityManager createEntityManager() {
-		 return em;
-	 }
-	 
-	 public void close(@Disposes EntityManager em) {
-	      if(em.isOpen()) {
-	          em.close();
-	       }
-	 }
-	
-
-    public static void main(String[] args) {
-    	EntityManagerFactory emf = Persistence.createEntityManagerFactory("persistence-unit-filme");
-    	emf.createEntityManager();
-    	emf.close();
+	@Produces
+	public EntityManager createEntityManager() {
+		return em;
 	}
-    
-    
-    
+
+	public void close(@Disposes EntityManager em) {
+		if (em.isOpen()) {
+			em.close();
+		}
+	}
+
 }
